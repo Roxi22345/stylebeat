ba0b5ec1c43342e01d6ae59cd53a4a6d58a0dd0c
RyuMika <pantirudenis@gmail.com>
2025-04-05 16:59:02 +0300

    Delete siteus directory

--- siteus/script.js
+++
@@ -1,8 +0,0 @@
-const toggleBtn = document.getElementById("toggle-theme");
-toggleBtn.addEventListener("click", () => {
-  const currentTheme = document.documentElement.getAttribute("data-theme");
-  document.documentElement.setAttribute(
-    "data-theme",
-    currentTheme === "dark" ? "light" : "dark"
-  );
-});
